16456c7d2f6c413e8ecdb09252014d1a9ba80a23
jared <jaredgold@proton.me>
2023-04-14 14:20:31 -0400

    implement validity system, determines valid state, calls error

--- main.js
+++ main.js
@@ -12,26 +12,49 @@ const htmlElements = (function () {
   elements.forEach((element) => {
     elementStorage[element] = document.getElementById(`${element}`);
 
-    elementStorage[element].addEventListener('focusout', validate);
-    elementStorage[element].addEventListener('keydown', validate);
+    elementStorage[element].addEventListener('focusout', validateQueue);
+    elementStorage[element].addEventListener('keydown', validateQueue);
 
   });
   return { elementStorage }
 }());
 
-function error(element) {
-// if error is on, validate here
+function error(e, errorMsg) {
+  if(e.srcElement.validity.valueMissing) {
+    errorMsg.textContent = 'Required'
+  }
+  // shit coming here HAS an error. it's aleady proven.
+  // this to determine what error is, print it, give it the error class.
+  // console.log(errorMsg, 'error');
+  // e.srcElement.classList.toggle('error')
+  // errorMsg.textContent = 'error bro'
+  // this should 
+  if(e.srcElement.className === 'error') {
+    return;
+  }
 
-// if error is off, validate onexit
+  e.srcElement.classList.toggle('error')
+ 
 }
+function validateQueue(e) {
+  const errorMsg = e.srcElement.nextSibling;
+  console.log(errorMsg, 'errorMsg')
+  console.log(e.srcElement.className, 'here')
+  // console.log(e.srcElement.nextSibling);
+  console.log(e.type);
+  if(errorMsg.classList === 'error' && e.type === 'keypress') { // if error is on
+    validate(e)
+  } else if(e.type === 'focusout') { // if error is on
+    validate(e)
+  }
+  function validate(e) {
+    if(!e.srcElement.validity.valid) {
+      error(e, errorMsg)
+    } else {
+      return
+    }
 
-function validate(e) {
-  console.log(e.srcElement.validity.valid)
-  console.log(e.srcElement.validity)
-  // if error is on, validate here
-
-  // if error is off, validate onexit
-
+  }
 
 }
 
