7a074de54f4549f099cb645366490bedcfdc0a85
jared <jaredgold@proton.me>
2023-04-13 23:51:42 -0400

    add eventlisteners to htmlelements, initialize error and validate func

--- main.js
+++ main.js
@@ -11,8 +11,29 @@ const htmlElements = (function () {
   const elements = ['email', 'country', 'zip', 'password', 'password-confirm'];
   elements.forEach((element) => {
     elementStorage[element] = document.getElementById(`${element}`);
+
+    elementStorage[element].addEventListener('focusout', validate);
+    elementStorage[element].addEventListener('keydown', validate);
+
   });
   return { elementStorage }
 }());
 
+function error(element) {
+// if error is on, validate here
+
+// if error is off, validate onexit
+}
+
+function validate(e) {
+  console.log(e.srcElement.validity.valid)
+  console.log(e.srcElement.validity)
+  // if error is on, validate here
+
+  // if error is off, validate onexit
+
+
+}
+
+htmlElements.elementStorage.email.style.backgroundColor = 'red';
 
