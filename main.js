922710d4b0df115325b1aa32d5a0642bbce19d3f
jared <jaredgold@proton.me>
2023-04-16 01:01:53 -0400

    reuse killError with function, integrate zip val and error with rest of system

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-/*
-ok i need
-my html inputs/select
-my validator
-my error
-
-*/
 const htmlElements = (function () {
   'use strict';
   const elementStorage = {};
@@ -19,7 +12,7 @@ const htmlElements = (function () {
   return { elementStorage }
 }());
 
-function error(e, errorMsg) {
+function error(e, errorMsg, zipError) {
   if(e.srcElement.validity.valueMissing) {
     errorMsg.textContent = 'Required';
   } else if(e.srcElement.validity.tooShort) {
@@ -27,70 +20,76 @@ function error(e, errorMsg) {
   } else if(e.srcElement.validity.typeMismatch) {
     errorMsg.textContent = 'Follow correct format john@smith.com' 
   }
+
+  if(e.srcElement.id === 'zip') {
+    errorMsg.textContent = `${zipError}`;
+    console.log(errorMsg)
+  }
+
+
   if(e.srcElement.className === 'error') {
     return;
   }
-
-  if(e === 'zip') {
-    
-  }
-
   e.srcElement.classList.toggle('error')
  
 }
 
 function validate(e) {
- function zipVal(country, zip) {
-  console.log(country)
-  console.log(zip)
-  // we need to set the zip input to either valid or invalid
-  // based on the value it has, and the country code supplied
-  const constraints = {
-    ch: [
-      "^(CH-)?\\d{4}$",
-      "Switzerland ZIPs must have exactly 4 digits: e.g. CH-1950 or 1950",
-    ],
-    fr: [
-      "^(F-)?\\d{5}$",
-      "France ZIPs must have exactly 5 digits: e.g. F-75012 or 75012",
-    ],
-    de: [
-      "^(D-)?\\d{5}$",
-      "Germany ZIPs must have exactly 5 digits: e.g. D-12345 or 12345",
-    ],
-    nl: [
-      "^(NL-)?\\d{4}\\s*([A-RT-Z][A-Z]|S[BCE-RT-Z])$",
-      "Netherland ZIPs must have exactly 4 digits, followed by 2 letters except SA, SD and SS",
-    ],
-    us: [
-      "^\\d{5}$",
-      "United States ZIPs must have exactly 5 digits"
-    ]
-  };
-// wwe got country, we got zip. based on country, we run the zip against its regex.
-// const result = constraints[country][0].test('zip');
-// console.log(constraints[country][0].test());
-  const constraint = new RegExp(constraints[country][0], "");
-  console.log(constraint);
-  const result = constraint.test(zip)
-  console.log(result)
-  if(result === false) {
-    error('zip', )
+  function killError() {
+    if(e.srcElement.className === 'error') {
+      e.srcElement.classList.toggle('error');
+      }
+      errorMsg.textContent = '';
+   
   }
-} 
-const errorMsg = e.srcElement.nextElementSibling;
+  function zipVal(country, zip) {
+    console.log(country)
+    console.log(zip)
+    // we need to set the zip input to either valid or invalid
+    // based on the value it has, and the country code supplied
+    const constraints = {
+      ch: [
+        "^(CH-)?\\d{4}$",
+        "Switzerland ZIPs must have exactly 4 digits: e.g. CH-1950 or 1950",
+      ],
+      fr: [
+        "^(F-)?\\d{5}$",
+        "France ZIPs must have exactly 5 digits: e.g. F-75012 or 75012",
+      ],
+      de: [
+        "^(D-)?\\d{5}$",
+        "Germany ZIPs must have exactly 5 digits: e.g. D-12345 or 12345",
+      ],
+      nl: [
+        "^(NL-)?\\d{4}\\s*([A-RT-Z][A-Z]|S[BCE-RT-Z])$",
+        "Netherland ZIPs must have exactly 4 digits, followed by 2 letters except SA, SD and SS",
+      ],
+      us: [
+        "^\\d{5}$",
+        "United States ZIPs must have exactly 5 digits"
+      ]
+    }; 
+
+    const constraint = new RegExp(constraints[country][0], "");
+    console.log(constraint);
+    const result = constraint.test(zip)
+    console.log(result)
+    if(result === false) {
+      error(e, errorMsg, constraints[country][1])
+    } else {
+      killError();
+    }
+  } 
+  const errorMsg = e.srcElement.nextElementSibling;
   if(e.srcElement.id === 'zip') {
     //
     zipVal(htmlElements.elementStorage.country.value, htmlElements.elementStorage.zip.value);
+    return;
   }
   if(!e.srcElement.validity.valid) {
     error(e, errorMsg)
   } else {
-    if(e.srcElement.className === 'error') {
-    e.srcElement.classList.toggle('error');
-
-    }
-    errorMsg.textContent = '';
+    killError();
   }
 
 }
